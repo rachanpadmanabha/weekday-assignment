1afa505926f0eb3cfa1e49997fcb535b69589f45
RACHAN K P <rachan.kp@juspay.in>
2024-05-09 21:32:16 +0530

    css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
-import { Typography, TextField, Select, MenuItem, Checkbox, FormControlLabel, Box, FormControl, Grid, InputLabel } from "@mui/material";
+import {
+  Typography,
+  TextField,
+  Select,
+  MenuItem,
+  Checkbox,
+  FormControlLabel,
+  Box,
+  FormControl,
+  Grid,
+  InputLabel,
+  useMediaQuery,
+} from "@mui/material";
 import JobCard from "./JobCard";
 import Loader from "./Loader";
-import "./App.css"
+import "./App.css";
 import fetchJobs, { cancelFetchJobs } from "./fetchJobsService";
 
 const App = () => {
@@ -11,7 +23,7 @@ const App = () => {
   const [error, setError] = useState(null);
   const [page, setPage] = useState(0);
   const [hasMore, setHasMore] = useState(true);
-
+  const isSmallScreen = useMediaQuery("(max-width:768px)");
   // State variables for filters
   const [locationFilter, setLocationFilter] = useState("");
   const [roleFilter, setRoleFilter] = useState("");
@@ -41,36 +53,45 @@ const App = () => {
     const parsedMinimum = parseFloat(minimum);
     return minSalary >= parsedMinimum && maxSalary <= parsedMinimum + 10;
   };
-  console.log("salaryFilter", salaryFilter)
+  console.log("salaryFilter", salaryFilter);
   const filteredJobs = jobs.filter((job) => {
-
     if (roleFilter && job.jobRole.toLowerCase() !== roleFilter.toLowerCase()) {
       return false;
     }
-
-    if (locationFilter && !job.location.toLowerCase().includes(locationFilter.toLowerCase()) && locationFilter !== "remote") {
+    if (
+      locationFilter &&
+      !job.location.toLowerCase().includes(locationFilter.toLowerCase()) &&
+      locationFilter !== "remote"
+    ) {
       return false;
     }
-
-    if (salaryFilter && !isSalaryInRange(job.minJdSalary, job.maxJdSalary, salaryFilter)) {
+    if (
+      salaryFilter &&
+      !isSalaryInRange(job.minJdSalary, job.maxJdSalary, salaryFilter)
+    ) {
       return false;
     }
-
-    if (companyFilter && !job.companyName.toLowerCase().includes(companyFilter.toLowerCase())) {
+    if (
+      companyFilter &&
+      !job.companyName.toLowerCase().includes(companyFilter.toLowerCase())
+    ) {
       return false;
     }
 
     return true;
   });
 
-
-
-
   return (
     <div className="app">
       <Typography variant="h2">Job Listings</Typography>
 
-      <Grid container spacing={2} alignItems="center" justifyContent={"center"}>
+      <Grid
+        container
+        spacing={2}
+        alignItems="center"
+        justifyContent={isSmallScreen ? "flex-start" : "center"}
+        style={{ marginLeft: isSmallScreen ? "10px" : "0" }}
+      >
         <Grid item>
           <Box sx={{ minWidth: 120 }}>
             <FormControl fullWidth>
@@ -102,7 +123,14 @@ const App = () => {
         </Grid>
         <Grid item>
           <FormControlLabel
-            control={<Checkbox checked={locationFilter === "Remote"} onChange={(e) => setLocationFilter(e.target.checked ? "Remote" : "")} />}
+            control={
+              <Checkbox
+                checked={locationFilter === "Remote"}
+                onChange={(e) =>
+                  setLocationFilter(e.target.checked ? "Remote" : "")
+                }
+              />
+            }
             label="Remote"
           />
         </Grid>
@@ -125,7 +153,6 @@ const App = () => {
                 <MenuItem value="50">50LPA</MenuItem>
                 <MenuItem value="60">60LPA</MenuItem>
                 <MenuItem value="70">Above 70LPA</MenuItem>
-
               </Select>
             </FormControl>
           </Box>
@@ -140,13 +167,14 @@ const App = () => {
         </Grid>
       </Grid>
 
-
       <InfiniteScroll
         dataLength={filteredJobs.length}
         next={fetchData}
         hasMore={hasMore}
         loader={<Loader />}
-        endMessage={<Typography variant="subtitle1">Yay! You have seen it all</Typography>}
+        endMessage={
+          <Typography variant="subtitle1">Yay! You have seen it all</Typography>
+        }
       >
         <div className="job-wrapper">
           {filteredJobs.map((job) => (
